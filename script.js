11b1f11ca07b091c8204243ace2acae536d8c6f3
Rob Selkowitz <121113624+RobSelkowitz@users.noreply.github.com>
2023-01-03 14:43:22 -0500

    define computer choice and score variables

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Hello Cleveland!");
+
+//Started by R Selkowitz Jan 3 2023
+
+let wins = 0;
+let losses = 0;
+let draws = 0;
+
+function computer_choice(){
+    choice=(1+Math.floor(Math.random()*3));
+    return (choice);
+
+}
